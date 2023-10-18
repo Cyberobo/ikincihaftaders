d47cc9f235211d14c351f499093ad78124f43f0c
aidata <aidata@10.121.3.189>
2023-10-18 10:11:22 +0300

    asdasd

--- javahaftakilders/src/javaikicnihaftaders/soru1.java
+++ javahaftakilders/src/javaikicnihaftaders/soru1.java
@@ -1,17 +1,26 @@
 package javaikicnihaftaders;
 
+import java.util.Scanner;
+
 
 public class soru1 {
 
 	public static void main(String[] args) {
+		Scanner tara=new Scanner(System.in);
+		
+		System.out.print("lutfen bir deger girin: ");
+		int num=tara.nextInt();
+		tekCiftBul(num);
 		
 	}
-
+	
+	public static void tekCiftBul(int num) {
+		if(num%2==0)
+			System.out.println("cift");
+		else
+			System.out.println("tek");
+		
+	}
+	
 }
 
-class hayvan{
-	
-	
-	
-	
-}
